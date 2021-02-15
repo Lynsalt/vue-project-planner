4924f43bb84767594cae42d7fcab2fd973f90b31
Lynden Saltibus <menelike3@gmail.com>
2021-02-15 18:20:41 -0500

    created the EditProject view in components directory

---
+++ src/views/EditProject.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <form @submit.prevent="handleSubmit">
+      <label>Title:</label>
+      <input type="text" required v-model="title">
+      <label>Details:</label>
+      <textarea required  v-model="details"></textarea>
+      <button>Add Project</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+      return{
+
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
