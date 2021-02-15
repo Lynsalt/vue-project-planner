f06d8a349888307d2147e852037c0d41f254f09d
Lynden Saltibus <menelike3@gmail.com>
2021-02-15 18:19:16 -0500

    Added the navbar to the main app.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="main-nav">
+        <router-link :to="{name: 'Home'}">Projects</router-link>
+        <router-link :to="{name: 'AddProject'}">Add a new Project</router-link>
+    </nav>
+</template>
+<script>
+export default {
+    name: 'Navbar',
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+<style>
+.main-nav a{
+    display:inline-block;
+    text-decoration: none;
+    margin: 0 1em 0 0;
+    color:#fff;
+    font-size: 1.125rem;
+}
+a.router-link-active{
+    border-bottom:2px solid #00ce89;
+}
+</style>
