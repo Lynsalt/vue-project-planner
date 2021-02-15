2b17d54fce7ffe196d1a5a2749e81c4aba50839d
Lynden Saltibus <menelike3@gmail.com>
2021-02-15 16:01:06 -0500

    files added to repot

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+   <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <Singleproject :project="project" @delete="handleDelete" @complete="handleComplete"/>
+      </div>
+   </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import Singleproject from '../components/Singleproject'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: {Singleproject},
+  data(){
+    return{
+      projects: []
+    }
+  },
+  mounted(){
+    fetch(' http://localhost:3000/projects')
+    .then(res => res.json())
+    .then(data => this.projects = data)
+    .catch(err => console.log(err.message))
+  },
+  methods:{
+    handleDelete(id){
+       this.projects = this.projects.filter(project =>{
+         return project.id !== id
+       })
+    },
+    handleComplete(id){
+      let pro = this.projects.find(project =>{
+        return project.id === id
+      })
+      pro.complete = !pro.complete
+    }
   }
+
 }
 </script>
+<style scoped>
+.container{
+  max-width:600px;
+  margin:0 auto;
+}
+</style>
