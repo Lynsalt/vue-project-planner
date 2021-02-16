a1b7045b847aa525fe018dfaa2dee1efa4673136
Lynden Saltibus <menelike3@gmail.com>
2021-02-15 19:03:24 -0500

    Fetch data to pre populate form

--- src/views/EditProject.vue
+++ src/views/EditProject.vue
@@ -1,22 +1,33 @@
 <template>
   <div class="container">
-    <form @submit.prevent="handleSubmit">
+    <form>
       <label>Title:</label>
       <input type="text" required v-model="title">
       <label>Details:</label>
       <textarea required  v-model="details"></textarea>
-      <button>Add Project</button>
+      <button>Edit Project</button>
     </form>
   </div>
 </template>
 
 <script>
 export default {
+    props:['id'],
     name:' EditProject',
   data(){
       return{
-
+         title: '',
+         details: '',
+         uri: 'http://localhost:3000/projects/' + this.id
       }
+  },
+  mounted(){
+       fetch(this.uri)
+       .then(res=> res.json())
+       .then(data=>{
+           this.title = data.title
+           this.details = data.details
+       })
   }
 }
 </script>
