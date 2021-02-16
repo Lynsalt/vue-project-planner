50f8ccca1f29346f00bb800ac56f7db09be9d6da
Lynden Saltibus <menelike3@gmail.com>
2021-02-16 03:38:59 -0500

    Implemented footer component.

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer class="container-fluid">
+      <div class="container copyright"><span>{{ new Date().getFullYear() }}</span></div>
+  </footer>
+</template>
+
+<script>
+name: 'Footer'
+export default {
+
+}
+</script>
+
+<style>
+   footer{    
+       padding:1em 0;
+       background-color:#444;
+   }
+   .copyright, a{
+       text-align:center;
+       color:antiquewhite;
+   }
+   a{
+       text-decoration:none;
+       color:#00ce89;
+       font-size:.9rem;
+   }
+</style>
